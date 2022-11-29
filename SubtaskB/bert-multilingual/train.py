18698df8e1fd14b33090f54769d256ec82819794
chiranthans23 <chiranthans23@gmail.com>
2022-11-29 09:30:11 +0000

    mixed precision and some langugage specific EDA

--- SubtaskB/bert-multilingual/train.py
+++ SubtaskB/bert-multilingual/train.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 from config import config, seed_everything
 import pandas as pd
 import torch
-from model import BertMultiModel, loss_fn, score
+from model import BertMultiModel, loss_fn, score, multi_acc
 from dataset import get_train_val_loaders
 import gc
 import numpy as np
@@ -15,30 +15,32 @@ def train():
     '''
 
     train_data = pd.read_csv("../multilingual_train.tsv", sep='\t', names=['text', 'label'], header=0)
-    train_data = train_data[:1000]
+    
+    train_data = train_data[:10000]
 
     skf = StratifiedKFold(n_splits=config['folds'], shuffle=True, random_state=config['seed'])
     train_data['text'] = train_data['text'].astype(str)
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-
+    scaler = torch.cuda.amp.GradScaler()
 
     for fold, (train_idx, val_idx) in enumerate(skf.split(train_data, train_data.label), start=1): 
         
         print(f'Fold: {fold}')
         model = BertMultiModel()
 
-        param_optimizer = list(model.named_parameters())
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        optimizer_parameters = [
-            {'params': [p for n, p in param_optimizer
-                        if not any(nd in n for nd in no_decay)],
-            'weight_decay': config['weight_decay']},
-            {'params': [p for n, p in param_optimizer
-                        if any(nd in n for nd in no_decay)],
-            'weight_decay': 0.0}]
+        # param_optimizer = list(model.named_parameters())
+        # no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        # optimizer_parameters = [
+        #     {'params': [p for n, p in param_optimizer
+        #                 if not any(nd in n for nd in no_decay)],
+        #     'weight_decay': config['weight_decay']},
+        #     {'params': [p for n, p in param_optimizer
+        #                 if any(nd in n for nd in no_decay)],
+        #     'weight_decay': 0.0}]
 
-        optimizer = config['optimizer'](optimizer_parameters, lr=config['learning_rate'], betas=(0.9, 0.999))
+        train_loss_epoch, val_loss_epoch = [], []
+        optimizer = config['optimizer'](model.parameters(), lr=config['learning_rate'], betas=(0.9, 0.999))
         criterion = loss_fn()   
         dataloaders_dict = get_train_val_loaders(train_data, train_idx, val_idx, config['batch_size'])
         train_dataloader, val_dataloader = dataloaders_dict['train'], dataloaders_dict['val']
@@ -50,6 +52,8 @@ def train():
 
         gc.collect()
         torch.cuda.empty_cache()
+
+
         for epoch_num in range(config['num_epochs']):
                 total_loss_train = 0
                 accum_iter = 8
@@ -64,25 +68,35 @@ def train():
 
 
                     with torch.set_grad_enabled(True):
-                        output = model(input_id, mask)
-                        batch_loss = criterion(output, train_label.long())
+                        with torch.cuda.amp.autocast():
+                            output = model(input_id, mask)
+                            batch_loss = criterion(output, train_label.long())
                         total_loss_train += batch_loss.item()
-                        preds.append(torch.max(torch.log_softmax(output, dim=1).cpu().detach(),dim=1)[1].numpy())
+                        train_loss_epoch.append(batch_loss.item())
+
+                        # print(f"output from the model : {output}")
+                        log_softmax = torch.log_softmax(output, dim=1).cpu().detach()
+                        #print(f"output from log softmax {log_softmax}")
+                        tor_max = torch.max(log_softmax, dim=1)[1]
+                        #print(f"output from torch max {tor_max}")
+                        preds.append(tor_max.numpy())
                         actual.append(train_label.long().cpu().detach().numpy())
 
                         model.zero_grad()
                         batch_loss /= accum_iter
-                        batch_loss.backward()
+                        scaler.scale(batch_loss).backward()
                         scheduler.step()
+
                         if ((b_id + 1) % accum_iter == 0) or (b_id + 1 == len(train_dataloader)):
-                            optimizer.step()
-                            optimizer.zero_grad()
+                            scaler.step(optimizer)
+                            optimizer.zero_grad(set_to_none=True)
+                            scaler.update()
                             
                         gc.collect()
                         torch.cuda.empty_cache()
 
                 train_metrics = score(np.concatenate(preds),np.concatenate(actual))
-
+                train_acc = multi_acc(np.concatenate(preds),np.concatenate(actual))
                 total_loss_val = 0
                 preds, actual = [], []
 
@@ -102,21 +116,25 @@ def train():
 
                         batch_loss = criterion(output, val_label.long())
                         total_loss_val += batch_loss.item()
+                        val_loss_epoch.append(batch_loss.item())
                         
                 val_metrics = score(np.concatenate(preds),np.concatenate(actual))
-                        
+                val_acc = multi_acc(np.concatenate(preds),np.concatenate(actual))
                 
                 print(
                     f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_dataloader.dataset): .3f} \
+                    | Train Accuracy: {train_acc} \
                     | Train Metrics (Precision, Recall, F1-Score): {train_metrics} \
                     | Val Loss: {total_loss_val / len(val_dataloader.dataset): .3f} \
+                    | Val Accuracy: {val_acc} \
                     | Val Metrics (Precision, Recall, F1-Score): {val_metrics}')
                   
                 torch.save(model.state_dict(), f"./models/{config['model_name']}_FOLD{fold}.pth")
-
+                
+    return train_loss_epoch, val_loss_epoch
 
 if __name__ == '__main__':
     seed_everything(config['seed'])
     logging.set_verbosity_warning()
     logging.set_verbosity_error()
-    train()
+    ts_epoch, vs_epoch = train()
